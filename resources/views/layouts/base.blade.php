53b7065e50032c434510fdbdc97872cf5a7bc881
mastadjayy <dlionelmederic@gmail.com>
2022-02-21 13:56:00 +0000

    Landing page update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,7 +12,7 @@
     <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-  />
+    />
   <!--Animate On Scroll CSS-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -213,15 +213,19 @@
                 <div class="rounded-lg h-64 overflow-hidden">
                   <img alt="content" class="object-cover object-center h-full w-full" src="https://shtheme.org/demosd/agroly/wp-content/uploads/2020/12/gallery-3.jpg">
                 </div>
-                <h6 class="title-font text-xl font-medium text-slate-300 mt-6 mb-3 uppercase"><a href="#">Substrat-GT</a></h6>
-                <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+                <h6 class="title-font text-xl font-medium text-white mt-6 mb-3 uppercase"><a href="#">Substrat-GT</a></h6>
+                <p class="leading-relaxed text-slate-300">
+                  Le Substrat-GT est un produit utilisé dans la technique de culture en pots. Ce substrat  à la fois aéré, drainant, stable et rétenteur d’eau favorise le développement racinaire des jeunes plants.
+                </p>
               </div>
               <div class="sm:w-6/12 mb-10 px-4">
                 <div class="rounded-lg h-64 overflow-hidden">
                   <img alt="content" class="object-cover object-center h-full w-full" src="https://shtheme.org/demosd/agroly/wp-content/uploads/2020/12/gallery-3.jpg">
                 </div>
-                <h6 class="title-font text-xl font-medium text-slate-300 mt-6 mb-3 uppercase"><a href="#">Cocochips-GT</a></h6>
-                <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+                <h6 class="title-font text-xl font-medium text-white mt-6 mb-3 uppercase"><a href="#">Cocochips-GT</a></h6>
+                <p class="leading-relaxed text-slate-300">
+                  Le Cocochips-GT est un produit utilisé dans différents types de culture. Ce produit  à la fois aéré, drainant, stable et détenteur d’eau favorise un bon enracinement de la plante.
+                </p>
               </div>
             </div>
           </div>
@@ -297,8 +301,8 @@
       </div>
     </section>
 
-
-    <section class="relative py-20 bg-green-600">
+    <!--
+    <section class="relative py-20 bg-white">
       <div
         class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
         style="height: 80px;"
@@ -313,7 +317,7 @@
           y="0"
         >
           <polygon
-            class="text-green fill-current"
+            class="text-white fill-current"
             points="2560 0 2560 100 0 100"
           ></polygon>
         </svg>
@@ -332,7 +336,7 @@
               <div
                 class="text-amber-600 bg-amber-300 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full"
               >
-                <i class="fas fa-rocket text-xl"></i>
+                <i class="fa-duotone fa-user text-xl"></i>
               </div>
               <h3 class="text-3xl font-semibold">
                 Organic farmers are outstanding in their fields.
@@ -376,7 +380,7 @@
                     <div>
                       <span
                         class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-amber-600 bg-amber-200 mr-3"
-                        ><i class="far fa-paper-plane"></i
+                        ><i class="fa-duotone fa-user"></i
                       ></span>
                     </div>
                     <div>
@@ -390,89 +394,9 @@
         </div>
       </div>
     </section>
-    
-    <section class="pt-20 pb-48">
-      <div class="container mx-auto px-4">
-        <div class="flex flex-wrap justify-center text-center mb-24">
-          <div class="w-full lg:w-6/12 px-4">
-            <h2 class="text-4xl font-semibold">Here are our heroes</h2>
-            <p class="text-lg leading-relaxed m-4 text-gray-600">
-              According to the National Oceanic and Atmospheric
-              Administration, Ted, Scambos, NSIDClead scentist, puts the
-              potentially record maximum.
-            </p>
-          </div>
-        </div>
-        <div class="flex flex-wrap">
-          <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-            <div class="px-6">
-              <img
-                alt="..."
-                src="./assets/img/team-1-800x800.jpg"
-                class="shadow-lg rounded-full max-w-full mx-auto"
-                style="max-width: 120px;"
-              />
-              <div class="pt-6 text-center">
-                <h5 class="text-xl font-bold">Ryan Tompson</h5>
-                <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                  Web Developer
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-            <div class="px-6">
-              <img
-                alt="..."
-                src="./assets/img/team-2-800x800.jpg"
-                class="shadow-lg rounded-full max-w-full mx-auto"
-                style="max-width: 120px;"
-              />
-              <div class="pt-6 text-center">
-                <h5 class="text-xl font-bold">Romina Hadid</h5>
-                <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                  Marketing Specialist
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-            <div class="px-6">
-              <img
-                alt="..."
-                src="./assets/img/team-3-800x800.jpg"
-                class="shadow-lg rounded-full max-w-full mx-auto"
-                style="max-width: 120px;"
-              />
-              <div class="pt-6 text-center">
-                <h5 class="text-xl font-bold">Alexa Smith</h5>
-                <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                  UI/UX Designer
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-            <div class="px-6">
-              <img
-                alt="..."
-                src="./assets/img/team-4-470x470.png"
-                class="shadow-lg rounded-full max-w-full mx-auto"
-                style="max-width: 120px;"
-              />
-              <div class="pt-6 text-center">
-                <h5 class="text-xl font-bold">Jenna Kardi</h5>
-                <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                  Founder and CEO
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    -->
 
-    <section class="pb-20 relative block bg-gray-900">
+    <section class="pb-20 relative block bg-emerald-900">
       <div
         class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
         style="height: 80px;"
@@ -487,7 +411,7 @@
           y="0"
         >
           <polygon
-            class="text-gray-900 fill-current"
+            class="text-emerald-900 fill-current"
             points="2560 0 2560 100 0 100"
           ></polygon>
         </svg>
@@ -495,14 +419,13 @@
       <div class="container mx-auto px-4 lg:pt-24 lg:pb-64">
         <div class="flex flex-wrap text-center justify-center">
           <div class="w-full lg:w-6/12 px-4">
-            <h2 class="text-4xl font-semibold text-white">Build something</h2>
-            <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
-              Put the potentially record low maximum sea ice extent tihs year
-              down to low ice. According to the National Oceanic and
-              Atmospheric Administration, Ted, Scambos.
+            <h6 class="text-lg mb-2 font-semibold uppercase text-slate-100">Contactez-nous</h6>
+            <p class="text-lg leading-relaxed mt-4 text-slate-200">
+              Via ce formulaire
             </p>
           </div>
         </div>
+        <!--
         <div class="flex flex-wrap mt-12 justify-center">
           <div class="w-full lg:w-3/12 px-4 text-center">
             <div
@@ -545,9 +468,11 @@
             </p>
           </div>
         </div>
+        -->
       </div>
     </section>
-    <section class="relative block py-24 lg:pt-0 bg-gray-900">
+
+    <section class="relative block py-24 lg:pt-0 bg-emerald-900">
       <div class="container mx-auto px-4">
         <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">
           <div class="w-full lg:w-6/12 px-4">
@@ -555,19 +480,15 @@
               class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300"
             >
               <div class="flex-auto p-5 lg:p-10">
-                <h4 class="text-2xl font-semibold">Laissez-nous un message</h4>
-                <p class="leading-relaxed mt-1 mb-4 text-gray-600">
-                  Complete this form and we will get back to you in 24 hours.
-                </p>
-                <div class="relative w-full mb-3 mt-8">
+                <div class="relative w-full mb-3">
                   <label
                     class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                    for="full-name"
-                    >Full Name</label
+                    for="nom"
+                    >Nom</label
                   ><input
                     type="text"
                     class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                    placeholder="Full Name"
+                    placeholder="Nom"
                     style="transition: all 0.15s ease 0s;"
                   />
                 </div>
@@ -592,7 +513,7 @@
                     rows="4"
                     cols="80"
                     class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                    placeholder="Type a message..."
+                    placeholder="Votre message..."
                   ></textarea>
                 </div>
                 <div class="text-center mt-6">
@@ -601,7 +522,7 @@
                     type="button"
                     style="transition: all 0.15s ease 0s;"
                   >
-                    Send Message
+                    Envoyer
                   </button>
                 </div>
               </div>
